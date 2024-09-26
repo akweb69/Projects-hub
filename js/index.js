f45e2a958ce98f2b5b31a2aef0be163546d15968
Abu Kalam <akwebdev69@gmail.com>
2024-09-26 09:41:11 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function hoverLink() {
+  const linkBox = document.getElementById("live-link");
+  linkBox.addEventListener("click", function () {
+    window.location.href = "https://akweb69.github.io/Assaignment-01/";
+  });
+//   
+const linkBox2 = document.getElementById("live-link2");
+linkBox2.addEventListener("click", function () {
+  window.location.href = "https://akweb69.github.io/Assignment-2/";
+});
+}
+hoverLink();
+
+  
