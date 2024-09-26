435a0068ab15d2002637ad977eea490e43757e20
Abu Kalam <akwebdev69@gmail.com>
2024-09-27 00:32:02 +0600

    done

--- js/index.js
+++ js/index.js
@@ -8,7 +8,19 @@ const linkBox2 = document.getElementById("live-link2");
 linkBox2.addEventListener("click", function () {
   window.location.href = "https://akweb69.github.io/Assignment-2/";
 });
+// -03
+const linkBox3 = document.getElementById("live-link3");
+linkBox3.addEventListener("click", function () {
+  window.location.href = "https://akweb69.github.io/Assignment-03/";
+});
+// -04
+const linkBox4 = document.getElementById("live-link4");
+linkBox4.addEventListener("click", function () {
+  window.location.href = "https://akweb69.github.io/Assignment-05/";
+});
 }
 hoverLink();
+
+// 3
 
   
